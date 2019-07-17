219f54d55ad96837d55c0558ff87dde100ce3608
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-07-17 10:57:12 -0300

    Iniciando o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/">Tarefas</router-link>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/about">Agenda</router-link>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,14 +27,32 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  margin-top: 90px;
+}
+a {
+  color: aliceblue;
+  text-decoration: none;
+  font-weight: 900;
+}
+a:hover {
+  color: silver;
+  text-decoration: none;
+}
+.nav {
+  display: inline;
+}
+.nav-item {
+  float: left;
+  /* padding: 20px 30px; */
+}
+.navbar {
+  width: 100vw;
+  height: 60px;
+  background-color: #2c3e50;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: inline-block;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+  <form class="content">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form> 
   </div>
 </template>
+
+<style scoped>
+.content {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
