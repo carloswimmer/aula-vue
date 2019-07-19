799020df2835b8dc6c0b680273c67d23a2bf1791
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-07-19 11:33:33 -0300

    Criada tela de contato

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,27 +1,100 @@
 <template>
   <div class="container">
-  <form class="content">
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form> 
+    <form>
+      <div v-if="campoInserirVisivel">
+        <input type="text" name="nome" id="nome" v-model="novaPessoa.nome">
+        <input type="text" name="telefone" id="telefone" v-model="novaPessoa.telefone">
+        <button type="button" class="btn btn-primary" @click="insere(novaPessoa)">Inserir</button>
+      </div>
+      <div v-else>
+        <input type="text" v-model="nomeBuscado" placeholder="Digite para buscar um nome" @input="filtra(nomeBuscado)">
+        <button type="button" class="btn btn-primary" @click="mostraInserir(true)">Novo</button>
+      </div>
+    </form> 
+    <table>
+      <thead>
+        <th>Nome</th>
+        <th>telefone</th>
+      </thead>
+      <tbody v-for="pessoa in pessoasFiltradas" :key="pessoa.nome">
+        <td>{{ pessoa.nome }}</td>
+        <td>{{ pessoa.telefone }}</td>
+      </tbody>
+    </table>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      pessoas: [
+        {
+          nome: 'Carlos',
+          telefone: '2324-4324'
+        },
+        {
+          nome: 'Renata',
+          telefone: '4985-6636'
+        },
+        {
+          nome: 'Cuca',
+          telefone: '1876-6366'
+        }
+      ],
+      novaPessoa: {},
+      nomeBuscado: '',
+      campoInserirVisivel: false,
+      pessoasBuscadas: []
+    }
+  },
+  computed: {
+    pessoasFiltradas () {
+      return this.nomeBuscado ? this.pessoasBuscadas : this.pessoas
+    }
+  },
+  methods: {
+    filtra (termo) {
+      this.pessoasBuscadas = this.pessoas.filter(pessoa => pessoa.nome.includes(termo))
+    },
+    mostraInserir (boolean) {
+      this.campoInserirVisivel = boolean
+    },
+    insere (pessoa) {
+      this.pessoas.push(pessoa)
+      this.campoInserirVisivel = false
+      this.novaPessoa = {}
+      this.nomeBuscado = ''
+    }
+  }
+}
+</script>
+
+
 <style scoped>
-.content {
+.container {
   max-width: 600px;
   margin: 0 auto;
 }
+table {
+  width: 100%;
+  margin: 30px;
+
+}
+td {
+  border: 1px solid #666666;
+  padding: 10px;
+}
+input, button {
+  padding: 10px;
+  border-radius: 6px;
+  margin: 15px;
+}
+.fade-enter-active, .fade-leave-active {
+	transition: opacity .5s
+}
+.fade-enter, .fade-leave-to {
+	opacity: 0
+}
+
 </style>
